1921d47de2174be755b17db6418ed55f03fcd592
maurocua <mauro.cuassolo@gmail.com>
2023-05-09 21:10:57 -0300

    spring security 7

--- src/main/java/security/WebSecurityConfig.java
+++ src/main/java/security/WebSecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -15,7 +14,6 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
 public class WebSecurityConfig {
     
     @Bean
@@ -38,7 +36,7 @@ public class WebSecurityConfig {
     UserDetailsService userDetailsService(){
         InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
         manager.createUser(User.withUsername("admin")
-            .password(passwordEncoder().encode("Mermelada666"))
+            .password(passwordEncoder().encode("admin"))
             .roles()
             .build());
         return manager;
